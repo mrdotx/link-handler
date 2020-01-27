3163788e526ba83fbd6ef5b642d1b22dbc04b692
mrdotx <klassiker@gmx.de>
2020-01-27 13:00:34 +0100

    custom notify-send icons removed

--- link_handler.sh
+++ link_handler.sh
@@ -3,7 +3,7 @@
 # path:       ~/projects/newsboat/link_handler.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/newsboat
-# date:       2020-01-27T11:56:48+0100
+# date:       2020-01-27T12:42:44+0100
 
 movies="mpv --quiet"
 podcasts="$TERMINAL -e mpv --no-audio-display"
@@ -11,31 +11,31 @@ pictures="sxiv -a -s f"
 
 # if no url given open browser
 [ -z "$1" ] && {
-    notify-send -i "$HOME/projects/shell/icons/rss.png" "Newsboat" "No URL given, open browser..." \
+    notify-send "Newsboat" "No URL given, open browser..." \
         && "$BROWSER"
     exit
 }
 
 case "$1" in
 *mkv | *webm | *mp4 | *youtube.com/watch* | *youtube.com/playlist* | *youtu.be*)
-    notify-send -i "$HOME/projects/shell/icons/rss.png" "Newsboat" "Open URL in multimedia player:\n$1" \
+    notify-send "Newsboat" "Open URL in multimedia player:\n$1" \
         && eval tsp "$movies --input-ipc-server=/tmp/mpvsoc$(date +%s) $1 >/dev/null 2>&1" &
     ;;
 *mp3 | *flac | *opus)
-    notify-send -i "$HOME/projects/shell/icons/rss.png" "Newsboat" "Open URL in multimedia player:\n$1" \
+    notify-send "Newsboat" "Open URL in multimedia player:\n$1" \
         && eval tsp "$podcasts --input-ipc-server=/tmp/mpvsoc$(date +%s) $1 >/dev/null 2>&1" &
     ;;
 *png | *jpg | *jpe | *jpeg | *gif)
-    notify-send -i "$HOME/projects/shell/icons/rss.png" "Newsboat" "Open URL in picture viewer:\n$1" \
+    notify-send "Newsboat" "Open URL in picture viewer:\n$1" \
         && curl -sL "$1" >"/tmp/$(echo "$1" | sed "s/.*\///")" \
         && $pictures "/tmp/$(echo "$1" | sed "s/.*\///")" >/dev/null 2>&1 &
     ;;
 *)
     if [ -f "$1" ]; then
-        notify-send -i "$HOME/projects/shell/icons/rss.png" "Newsboat" "Open URL in editor:\n$1" \
+        notify-send "Newsboat" "Open URL in editor:\n$1" \
             && eval "$TERMINAL -e $EDITOR $1" &
     else
-        notify-send -i "$HOME/projects/shell/icons/rss.png" "Newsboat" "Open URL in browser:\n$1" \
+        notify-send "Newsboat" "Open URL in browser:\n$1" \
             && eval "$BROWSER $1 >/dev/null 2>&1" &
     fi
     ;;
