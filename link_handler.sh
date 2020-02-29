5038859882e6148661206c9c4b307143e9c99f86
mrdotx <klassiker@gmx.de>
2020-02-29 13:49:26 +0100

    documents added

--- link_handler.sh
+++ link_handler.sh
@@ -3,17 +3,18 @@
 # path:       ~/repos/newsboat/link_handler.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/newsboat
-# date:       2020-02-28T08:14:18+0100
+# date:       2020-02-29T13:48:16+0100
 
 web="$BROWSER"
 edit="$TERMINAL -e $EDITOR"
 podcast="$TERMINAL -e mpv --no-audio-display --input-ipc-server=/tmp/mpvsoc$(date +%s)"
 video="mpv --really-quiet --input-ipc-server=/tmp/mpvsoc$(date +%s)"
 picture="sxiv -a -s f"
+document="zathura"
 
-# if no url given open browser
+# if no file/url given open browser
 [ -z "$1" ] && {
-    notify-send "link handler" "no url given, open browser..." \
+    notify-send "link handler" "no url/file given, open browser..." \
         && eval "$web" &
     exit
 }
@@ -32,7 +33,11 @@ case "$1" in
             && curl -sL "$1" >"/tmp/$(printf "%s" "$1" | sed "s/.*\///")" \
             && eval "$picture /tmp/$(printf "%s" "$1" | sed "s/.*\///") >/dev/null 2>&1" &
     ;;
-    *)
+    *pdf | *ps | *djvu | *epub | *cbr | *cbz)
+        notify-send "link handler" "open url in document viewer:\n$1" \
+            && curl -sL "$1" >"/tmp/$(printf "%s" "$1" | sed "s/.*\///")" \
+            && eval "$document /tmp/$(printf "%s" "$1" | sed "s/.*\///") >/dev/null 2>&1" &
+    ;;    *)
         if [ -f "$1" ]; then
             notify-send "link handler" "open url in editor:\n$1" \
                 && eval "$edit $1 >/dev/null 2>&1" &
