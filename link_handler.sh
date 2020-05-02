3dc09ba8f7c02c5b17ec355a9b36b57ec0cc96b8
mrdotx <klassiker@gmx.de>
2020-05-03 01:52:18 +0200

    small optimizations

--- link_handler.sh
+++ link_handler.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/link-handler/link_handler.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/newsboat
-# date:       2020-04-29T11:09:37+0200
+# date:       2020-05-03T01:50:12+0200
 
 web="$BROWSER"
 edit="$TERMINAL -e $EDITOR"
@@ -15,9 +15,9 @@ download="$TERMINAL -e aria2c"
 
 data="$1"
 
-# if no url/file given exit the application
+# if no url or file given exit the application
 [ -z "$data" ] && {
-    notify-send "link handler" "no url/file given, exit..."
+    notify-send "link handler [error]" "no url or file given, exit..."
     exit
 }
 
@@ -26,40 +26,61 @@ open() {
     eval "$2" "$1 $data >/dev/null 2>&1" &
 }
 
-# download files to tmp directory before open it
+# download file to tmp directory before open it
 open_tmp() {
-    curl -sL "$data" >"/tmp/$(printf "%s" "$data" | sed "s/.*\///")" \
-    && eval "$1 /tmp/$(printf "%s" "$data" | sed "s/.*\///") >/dev/null 2>&1" &
+    curl -sL "$data" >"/tmp/$(printf "%s" "$data" \
+        | sed "s/.*\///")" \
+        && eval "$1 /tmp/$(printf "%s" "$data" \
+        | sed "s/.*\///") >/dev/null 2>&1" &
 }
 
-# shellcheck disable=SC1001
 case "$data" in
-    *mkv | *mp4 | *webm | *youtube.com/watch* | *youtube.com/playlist* | *youtu.be*)
-        notify-send "link handler" "open url in video player:\n$data" \
+    *mkv \
+        | *mp4 \
+        | *webm \
+        | *'youtube.com/watch'* \
+        | *'youtube.com/playlist'* \
+        | *'youtu.be'*)
+        notify-send "link handler [video]" "$data" \
             && open "$video" "tsp"
     ;;
-    *mp3 | *flac | *opus)
-        notify-send "link handler" "open url in audio player:\n$data" \
+    *mp3 \
+        | *flac \
+        | *opus)
+        notify-send "link handler [audio]" "$data" \
             && open "$podcast" "tsp"
     ;;
-    *jpg | *jpe | *jpeg | *png | *gif | *webp)
-        notify-send "link handler" "open url in picture viewer:\n$data" \
+    *jpg \
+        | *jpe \
+        | *jpeg \
+        | *png \
+        | *gif \
+        | *webp)
+        notify-send "link handler [picture]" "$data" \
             && open_tmp "$picture"
     ;;
-    *pdf | *ps | *djvu | *epub | *cbr | *cbz)
-        notify-send "link handler" "open url in document reader:\n$data" \
+    *pdf \
+        | *ps \
+        | *djvu \
+        | *epub \
+        | *cbr \
+        | *cbz)
+        notify-send "link handler [document]" "$data" \
             && open_tmp "$document"
     ;;
-    *torrent | magnet\:* | *metalink | *iso)
-        notify-send "link handler" "open url in downloader:\n$data" \
+    *torrent \
+        | 'magnet\:'* \
+        | *metalink \
+        | *iso)
+        notify-send "link handler [download]" "$data" \
             && open "$download"
     ;;
     *)
         if [ -f "$data" ]; then
-            notify-send "link handler" "open url in editor:\n$data" \
+            notify-send "link handler [edit]" "$data" \
                 && open "$edit"
         else
-            notify-send "link handler" "open url in browser:\n$data" \
+            notify-send "link handler [web]" "$data" \
                 && open "$web"
         fi
     ;;
