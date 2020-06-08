61045c015901c7cc0b9befd8d4da128a296a0374
mrdotx <klassiker@gmx.de>
2020-06-08 14:10:54 +0200

    minor changes

--- link_handler.sh
+++ link_handler.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/link-handler/link_handler.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/link-handler
-# date:       2020-06-06T09:21:30+0200
+# date:       2020-06-08T11:11:47+0200
 
 web="$BROWSER"
 edit="$TERMINAL -e $EDITOR"
@@ -13,30 +13,30 @@ picture="sxiv -a -s f"
 document="$READER"
 download="$TERMINAL -e aria2c"
 
-data="$1"
+input="$1"
 
 # if no url or file given exit the script
-if [ -z "$data" ]; then
+if [ -z "$input" ]; then
     exit 1
 else
-    notify-send "link handler -> try to open" "$data"
+    notify-send "link handler -> try to open" "$input"
 fi
 
 # open in application and if given, open with tsp (taskspooler)
 open() {
-    eval "$2" "$1 $data >/dev/null 2>&1" &
+    eval "$2" "$1 $input >/dev/null 2>&1" &
 }
 
 # download file to tmp directory before open it
 open_tmp() {
-    curl -sL "$data" >"/tmp/$(printf "%s" "$data" \
+    curl -sL "$input" >"/tmp/$(printf "%s" "$input" \
         | sed "s/.*\///")" \
-        && eval "$1 /tmp/$(printf "%s" "$data" \
+        && eval "$1 /tmp/$(printf "%s" "$input" \
         | sed "s/.*\///") >/dev/null 2>&1" &
 }
 
 
-case "$data" in
+case "$input" in
     *mkv \
         | *mp4 \
         | *webm \
@@ -74,7 +74,7 @@ case "$data" in
             open "$download"
     ;;
     *)
-        if [ -f "$data" ]; then
+        if [ -f "$input" ]; then
             open "$edit"
         else
             open "$web"
